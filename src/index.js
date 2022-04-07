e7412899b870e8dbe1feba12ddde6862c5d8c46f
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-04-07 17:12:52 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import routes from "./server/routes.js";
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import { readFile } from 'fs/promises';
-
+import compression from 'compression';
 const json = JSON.parse(
   await readFile(
     new URL('./swagger.json', import.meta.url)
@@ -14,21 +14,20 @@ const json = JSON.parse(
 SyncDatabase();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://lucasfernandodev.github.io/Oscar-nominated/", credentials: true }))
+app.use(compression());
+
+
 app.use(express.json());
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(json))
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
-
 
 app.use(routes);
 app.listen(3333)
+
+
+// Impede que a aplicação não quebre caso um erro não tratado aconteça
+process.on('uncaughtException', (error) => console.log(`unhandledRejection happened: ${error.stack || error}`));
+process.on('unhandledRejection', (error) => console.log(`unhandledRejection happened: ${error.stack || error}`))
 
 
